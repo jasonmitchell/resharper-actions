196a57e63c51b83769abdbe207f1107f9437d445
Jason Mitchell <jasonmitchell@live.co.uk>
2019-10-06 21:11:09 +0100

    Making inspection thresholds optional

--- actions/resharper-inspection-threshold/index.js
+++ actions/resharper-inspection-threshold/index.js
@@ -3,14 +3,14 @@ const core = require('@actions/core');
 async function run() {
   try { 
     const summary = JSON.parse(core.getInput('results-summary-json'));
-    const errorThreshold = Number(core.getInput('error-threshold'));
-    const warningThreshold = Number(core.getInput('warning-threshold'));
+    const errorThreshold = core.getInput('error-threshold');
+    const warningThreshold = core.getInput('warning-threshold');
 
-    if (summary.severities.error > errorThreshold) {
+    if (errorThreshold && summary.severities.error > Number(errorThreshold)) {
       core.setFailed(`${summary.severities.error} Resharper errors were detected.  The acceptable threshold is ${errorThreshold}`)
     }
 
-    if (summary.severities.warning > warningThreshold) {
+    if (warningThreshold && summary.severities.warning > Number(warningThreshold)) {
       core.setFailed(`${summary.severities.warning} Resharper warnings were detected.  The acceptable threshold is ${warningThreshold}`)
     }
   } 
